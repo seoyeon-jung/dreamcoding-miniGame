a8a60690b0e79aa75d67162f70168b8f6dc56683
seoyeon-jung <jungluchia1204@gmail.com>
2022-08-17 16:09:20 +0900

    fix: fetch 오류 수정

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // Fetch the items from the JSON file
 function loadItems() {
     return fetch('data/data.json')
-    .them(response => response.json())
-    .them(json => json.items);
+    .then((response) => response.json()) // json으로 변환
+    .then((json) => json.items);  // json 안의 items를 return
 }
 
 // Update the list with the given items
 function displayItems(items) {
     const container = document.querySelector('.items');
-    container.innerHTML = items.map(item => createHTMLString(item).join(''));
+    container.innerHTML = items.map((item) => createHTMLString(item)).join('');
     // html li 요소로 전환 (createHTMLString 함수 이용)
     // join > 문자열의 배열을 한 가지의 문자열로 병합
 }
@@ -16,11 +16,11 @@ function displayItems(items) {
 // Create HTML list item from the given data item
 function createHTMLString(item) {
     return `
-    <li class="item">
-        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
-        <span class="item__description">${item.gender}, ${item.size}</span>
-      </li>
-    `;
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+        `;
 }
 
 // button 클릭되었을 떄 발생하는 이벤트
@@ -33,20 +33,21 @@ function onButtonClick (event, items) {
         return;
     }
 
-    dispalyItems(items.filter(item => item[key] === value));
+    const filtered = items.filter((item) => item[key] === value);
+    displayItems(filtered);
 }
 
 function setEventListeners(items) {
-    const logo = docuemnt.querySelector('.logo');
-    const buttons = docuemnt.querySelector('.buttons');
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
     logo.addEventListener('click', () => displayItems(items));
-    buttons.addEventListener('click', () => onButtonClick(event, item));
+    buttons.addEventListener('click', (event) => onButtonClick(event, items));
 }
 
 // main
 loadItems()   // data를 읽어와서 아이템을 전달하는 역할
-.them(items => {
-    dispalyItems(items);
+.then(items => {
+    displayItems(items);
     setEventListeners(items)
 })
 .catch(console.log);
